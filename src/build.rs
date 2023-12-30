c004a291e67b984c3d5bc8c0bff57abba5f009da
yuma14 <yuma140902@gmail.com>
2023-12-30 16:09:11 +0900

    feat: record input and output files

--- src/build.rs
+++ src/build.rs
@@ -5,30 +5,33 @@ use rlt::cli::BuildArgs;
 pub fn build(_args: BuildArgs) {
     let mut command = Command::new("uplatex");
     command.args(&[
-        "main.ltx",
         "-halt-on-error",
         "-interaction=nonstopmode",
         "-file-line-error",
+        "-recorder",
+        "main.ltx",
     ]);
     let mut process = command.spawn().expect("Failed to run uplatex");
     process.wait().expect("Failed to wait uplatex");
 
     let mut command = Command::new("uplatex");
     command.args(&[
-        "main.ltx",
         "-halt-on-error",
         "-interaction=nonstopmode",
         "-file-line-error",
+        "-recorder",
+        "main.ltx",
     ]);
     let mut process = command.spawn().expect("Failed to run uplatex");
     process.wait().expect("Failed to wait uplatex");
 
     let mut command = Command::new("uplatex");
     command.args(&[
-        "main.ltx",
         "-halt-on-error",
         "-interaction=nonstopmode",
         "-file-line-error",
+        "-recorder",
+        "main.ltx",
     ]);
     let mut process = command.spawn().expect("Failed to run uplatex");
     process.wait().expect("Failed to wait uplatex");
