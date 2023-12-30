84ff4ca8671858d3bfac99e8dc10b332211f05fa
yuma14 <yuma140902@gmail.com>
2023-12-30 14:18:29 +0900

    add build subcommand

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Debug, Parser)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[command(subcommand)]
+    subcommand: Commands,
+}
+
+#[derive(Debug, Subcommand)]
+enum Commands {
+    #[clap(alias = "b", about = "Build the document. [alias: b]")]
+    Build(BuildArgs),
+}
+
+#[derive(Debug, Parser)]
+struct BuildArgs {}
+
+impl Default for BuildArgs {
+    fn default() -> Self {
+        Self {}
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    dbg!(&args);
 }
