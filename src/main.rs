6815d841070ce7bf90cfae1cc2d8a6aaf10fc448
yuma14 <yuma140902@gmail.com>
2023-12-30 15:28:14 +0900

    fix: add command line options

--- src/main.rs
+++ src/main.rs
@@ -43,14 +43,34 @@ fn main() {
 
 fn build(_args: BuildArgs) {
     let mut command = Command::new("uplatex");
-    command.args(&["main.ltx"]);
-    command.spawn().expect("Failed to run uplatex");
+    command.args(&[
+        "main.ltx",
+        "-halt-on-error",
+        "-interaction=nonstopmode",
+        "-file-line-error",
+    ]);
+    let mut process = command.spawn().expect("Failed to run uplatex");
+    process.wait().expect("Failed to wait uplatex");
 
     let mut command = Command::new("uplatex");
-    command.args(&["main.ltx"]);
-    command.spawn().expect("Failed to run uplatex");
+    command.args(&[
+        "main.ltx",
+        "-halt-on-error",
+        "-interaction=nonstopmode",
+        "-file-line-error",
+    ]);
+    let mut process = command.spawn().expect("Failed to run uplatex");
+    process.wait().expect("Failed to wait uplatex");
 
     let mut command = Command::new("uplatex");
-    command.args(&["main.ltx"]);
-    command.spawn().expect("Failed to run uplatex");
+    command.args(&[
+        "main.ltx",
+        "-halt-on-error",
+        "-interaction=nonstopmode",
+        "-file-line-error",
+    ]);
+    let mut process = command.spawn().expect("Failed to run uplatex");
+    process.wait().expect("Failed to wait uplatex");
+
+    println!("done");
 }
